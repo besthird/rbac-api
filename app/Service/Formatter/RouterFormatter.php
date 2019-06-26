8fef585ec593fe262467cc4a904a21d9b5d73fd1
李铭昕 <715557344@qq.com>
2019-06-26 14:00:37 +0800

    Fixed bug.

--- app/Service/Formatter/RouterFormatter.php
+++ app/Service/Formatter/RouterFormatter.php
@@ -28,8 +28,8 @@ class RouterFormatter extends Formatter
             'name' => $model->name,
             'route' => $model->route,
             'method' => $model->method,
-            'created_at' => $model->created_at,
-            'updated_at' => $model->updated_at,
+            'created_at' => (string) $model->created_at,
+            'updated_at' => (string) $model->updated_at,
         ];
     }
 
